09979f023cc21f3217e78da13a089a2c1facaae6
Hugo <khachong2102005@gmail.com>
2025-06-30 17:22:45 +0700

    BaiTapLon

---
+++ QLHPSV/TuitionSys.Web/Areas/Student/Controllers/PaymentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TuitionSys.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+namespace TuitionSys.Web.Areas.Student.Controllers
+{
+    [Area("Student")]
+    public class PaymentController : Controller
+    {
+        private readonly IPaymentService _paymentService;
+  
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+           
+        }
+        public async Task<IActionResult> Index()
+        {
+            var studentId = HttpContext.Session.GetString("StudentId");
+
+            if (string.IsNullOrEmpty(studentId))
+            {
+                TempData["Error"] = "Không xác định được sinh viên.";
+                return RedirectToAction("Index");
+            }
+
+            var allPayments = await _paymentService.GetAllPaymentsAsync();
+            var studentPayments = allPayments
+                .Where(p => p.StudentId == studentId)
+                .OrderByDescending(p => p.PaymentDate);
+
+            return View("Index",studentPayments);
+        }
+       
+    }
+}
